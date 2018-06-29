ec7d87b8388091d31647c30e5f364e1d0e3083dc
issamelkbaiche <40404688+issamelkbaiche@users.noreply.github.com>
2018-06-29 16:22:54 -0400

    login/logout
    g

---
+++ sprint/mon-app/src/app/auth/auth0-variables.ts
@@ -0,0 +1,11 @@
+interface AuthConfig {
+  clientID: string;
+  domain: string;
+  callbackURL: string;
+}
+
+export const AUTH_CONFIG: AuthConfig = {
+  clientID: 'Dj36uHBRxi8U4vzkAExs8M8gScanglJq',
+  domain: 'issamelkbaiche.auth0.com',
+  callbackURL: 'http://localhost:4200/callback'
+};
